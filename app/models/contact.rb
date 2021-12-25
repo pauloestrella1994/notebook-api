60cd7cf543cf1a7ebf3eb665deae3a730f8794c8
pauloestrella1994 <pauloestrella1994@gmail.com>
2021-12-25 20:18:23 -0300

    Add kind_description in contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,14 @@
 class Contact < ApplicationRecord
     belongs_to :kind
+
+    def kind_description
+        self.kind.description
+    end
+
+    def as_json(options={})
+        super(
+            root: true,
+            methods: [:kind_description]
+        )
+    end
 end

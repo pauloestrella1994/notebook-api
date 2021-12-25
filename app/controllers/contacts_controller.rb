e6993b53fa10a922fd2f4348fcbeb257dae391b2
pauloestrella1994 <pauloestrella1994@gmail.com>
2021-12-25 20:50:55 -0300

    Permit kind to be created in post contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -46,6 +46,6 @@ class ContactsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def contact_params
-      params.require(:contact).permit(:name, :email, :birthday)
+      params.require(:contact).permit(:name, :email, :birthday, :kind_id)
     end
 end

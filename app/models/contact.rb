e025ef6afdfe4260ee20b9ba0649bdd8470ec8d4
pauloestrella1994 <pauloestrella1994@gmail.com>
2021-12-25 22:14:46 -0300

    Add change date with i18n in model and controller

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,6 +5,17 @@ class Contact < ApplicationRecord
         self.kind.description
     end
 
+    def to_br
+        {
+            name: self.name,
+            email: self.email,
+            birthday: (I18n.l(self.birthday) unless self.birthday.blank?),
+            created_at: (I18n.l(self.created_at)),
+            updated_at: (I18n.l(self.updated_at)),
+            kind: self.kind_description
+        }
+    end
+
     def as_json(options={})
         super(
             root: true,

06fd2cbbfb6b392747d8a51e50ec4541da250a8c
pauloestrella1994 <pauloestrella1994@gmail.com>
2022-01-16 22:18:05 -0300

    Remove method tha overwrite rails serializer

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,14 +4,4 @@ class Contact < ApplicationRecord
 
     accepts_nested_attributes_for :phones
 
-    def kind_description
-        self.kind.description
-    end
-
-    def as_json(options={})
-        super(
-            root: true,
-            methods: [:kind_description]
-        )
-    end
 end

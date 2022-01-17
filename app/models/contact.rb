e2d41d11cde45d8be82ac6cadb6b605e5f97505e
pauloestrella1994 <pauloestrella1994@gmail.com>
2022-01-16 21:28:16 -0300

    Remove unusuable def to_br

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,17 +8,6 @@ class Contact < ApplicationRecord
         self.kind.description
     end
 
-    def to_br
-        {
-            name: self.name,
-            email: self.email,
-            birthday: (I18n.l(self.birthday) unless self.birthday.blank?),
-            created_at: self.created_at,
-            updated_at: self.updated_at,
-            kind: self.kind_description
-        }
-    end
-
     def as_json(options={})
         super(
             root: true,

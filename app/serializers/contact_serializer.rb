820f4ebf201ccfef5924f2d2136066f2b67936a6
pauloestrella1994 <pauloestrella1994@gmail.com>
2022-01-28 09:46:02 -0300

    transform birthday serialization

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,3 +1,9 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :email, :birthday
+
+  def attributes(*args)
+    params = super(*args)
+    params[:birthday] = (I18n.l(object.birthday) unless object.birthday.blank?)
+    params
+  end
 end
